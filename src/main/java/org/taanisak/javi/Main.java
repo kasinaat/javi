0dcc61cb0668bd18c9a41a9ded23ec8ad5232bf4
kasinaat.ss <kasinaat.ss@zohocorp.com>
2022-09-04 22:50:17 +0530

    codecheck

--- src/main/java/org/taanisak/javi/Main.java
+++ src/main/java/org/taanisak/javi/Main.java
@@ -3,7 +3,6 @@ package org.taanisak.javi;
 import org.taanisak.javi.models.FileBuffer;
 import org.taanisak.javi.views.Editor;
 
-import java.io.Console;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
